93b198af05299ed2f6d63eb633b2ac68d6fe0ecb
Harrison <90717856+Jhegerton@users.noreply.github.com>
2022-05-07 17:16:04 -0400

    Created slideshow structure and added images.

---
+++ Javascript/react/484project/src/Slideshow.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Slide} from 'react-slideshow-image';
+import './Site.css';
+
+
+const slidePictures = [
+    {
+        url : './images/Altus.jpg',
+        caption: 'Altus Apartments'
+    },
+    {
+        url: './images/HamptonPlaza.jpg',
+        caption: 'Hampton Plaza'
+    },
+    {
+        url: './images/TheSoutherly.jpg',
+        caption: 'The Southerly'
+    },
+    {
+        url: './images/TowsonRow.jpg',
+        caption: 'Towson Row'
+    },
+    {
+        url:'./images/Promenade.jpg',
+        caption: "Towson Promenade"
+    },
+];
+
+const Slideshow = () => {
+        return (
+            <div className="slide-container">
+                <Slide>
+                    {slidePictures.map((slideImage, index) => (
+                        <div className="each-slide" key={index}>
+                            <div style={{'backgroundImage': `url(${slideImage.url})`}}>
+                                <span>{slideImage.caption}</span>
+                            </div>
+                        </div>
+                    ))}
+                </Slide>
+            </div>
+        );
+}
+
+export {Slideshow};
